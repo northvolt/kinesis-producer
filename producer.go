a65f54c26ccd8d3d71b386211b8c82cdf37918e1
Henrik Bondeson <henrik.bondeson@northvolt.com>
2021-05-19 11:20:01 +0200

    Removed logging after flushing records

--- producer.go
+++ producer.go
@@ -242,7 +242,7 @@ func (p *Producer) flush(records []*kinesis.PutRecordsRequestEntry, reason strin
 	defer p.semaphore.release()
 
 	for {
-		p.Logger.Info("flushing records", LogValue{"reason", reason}, LogValue{"records", len(records)})
+		//p.Logger.Info("flushing records", LogValue{"reason", reason}, LogValue{"records", len(records)})
 		out, err := p.Client.PutRecords(&kinesis.PutRecordsInput{
 			StreamName: &p.StreamName,
 			Records:    records,
